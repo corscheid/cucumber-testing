f099bc79afe9a1d59551505126ffaae64b367c88
Tirea Aean <tirea@learnnavi.org>
2017-09-29 19:24:39 -0400

    A lot of simplification

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,8 @@
+Given("I am on the home page") do
+  $browser.visit $home_url + '?m=0'
+end
+
+When(/^I click the "([^"]*)" link$/) do |text|
+  link = $browser.find('a',text: text)
+  $browser.visit link[:href]
+end
